8e88bfcc7f6c4af4101e61738b30a6a746f1db4a
ShreyaNayak7 <snayak@eng.ucsd.edu>
2019-05-10 13:35:17 -0700

    Updated file to read data from excel file and store into dataframe

--- gather_data.py
+++ gather_data.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def gather_data_from_csv(path_of_file):
     '''
     This function is used to gather data from a file that has an extension of "csv".
@@ -20,4 +21,13 @@ def gather_data_from_csv(path_of_file):
 
     return all_elements, columns, rows
 
-def gather_data_from_Excel_file(path_of_file):
+def gather_data_from_Excel_file(path_of_file, sh_name):
+    ''''
+    This function is used to gather data from an excel file into a dataframe
+    path_of_file: is the path of the excel file that conytains data
+    sh_name: name of the sheet that has the data
+    '''
+    assert isinstance(path_of_file,str)
+    assert isinstance(sh_name,str)
+    df=pd.read_excel(path_of_file, sheet_name=sh_name)
+    return df
