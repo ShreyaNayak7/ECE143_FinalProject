73f49d0850d26c718bee20288b42335d5ecc4292
unknown <akhil.jain.dfw.allen@gmail.com>
2019-05-10 03:46:29 -0700

    There are updates.

---
+++ README.md
@@ -0,0 +1,22 @@
+# README
+
+## Description of Submitted Python Code
+
+1. **first_plots.py**
+
+This file will eventually have code that includes functions for making our first plots.
+
+2. **gather_data.py**
+
+This file should be made to have functions that can be used to store data in accessible formats for plotting purposes.
+
+## Current and/or Recommended Direcories
+
+* Downloaded Data
+
+    * This directory should have raw data that is downloaded. Citations for this data should be included in this file.
+
+* Saved_Data
+    * This directory can include NumPy arrays and other forms of data that can be directly used in a Python program.
+
+## Citations for Downloaded Data in the Future "Downloaded_Data" directory.
---
+++ gather_data.py
@@ -0,0 +1,21 @@
+def gather_data_from_csv(path_of_file):
+    '''
+    This function is used to gather data from a file that has an extension of "csv".
+
+    :path_of_file: string
+    '''
+
+    assert isinstance(path_of_file,str)
+    file_1=open(path_of_file)
+    rows=file_1.readlines()
+    for counter_1 in range(len(rows)):
+        columns[counter_1]=rows[counter_1].split(",")
+    done=False
+    while(!done):
+        for counter_2 in range(len(len(rows))):
+            for counter_3 in range(len(len(colums(counter_2)))):
+                all_elements[number_of_elements]=columns[counter_2][counter_3]
+                number_of_elements=number_of_elements+1
+        done=True
+
+    return all_elements, columns, rows
