87b6f91fdf1b014bd9351377bae6309664350c28
ShreyaNayak7 <snayak@eng.ucsd.edu>
2019-05-17 13:14:03 -0700

    Updated file to read data from csv file and store into dataframe

--- gather_data.py
+++ gather_data.py
@@ -2,30 +2,19 @@ import pandas as pd
 def gather_data_from_csv(path_of_file):
     '''
     This function is used to gather data from a file that has an extension of "csv".
-
-    :path_of_file: string
+    
+    :path_of_file: string; is the path of the csv file that needs to be loaded into a dataframe
     '''
-
     assert isinstance(path_of_file,str)
-    file_1=open(path_of_file)
-    rows=file_1.readlines()
-    for counter_1 in range(len(rows)):
-        columns[counter_1]=rows[counter_1].split(",")
-    done=False
-    while( not done):
-        for counter_2 in range(len(len(rows))):
-            for counter_3 in range(len(len(colums(counter_2)))):
-                all_elements[number_of_elements]=columns[counter_2][counter_3]
-                number_of_elements=number_of_elements+1
-        done=True
-
-    return all_elements, columns, rows
+    df=pd.read_csv(path_of_file)
+    return df
 
 def gather_data_from_Excel_file(path_of_file, sh_name):
     ''''
     This function is used to gather data from an excel file into a dataframe
-    path_of_file: is the path of the excel file that conytains data
-    sh_name: name of the sheet that has the data
+    
+    :path_of_file: string; is the path of the excel file that contains data
+    :sh_name: string; name of the sheet that has the data
     '''
     assert isinstance(path_of_file,str)
     assert isinstance(sh_name,str)
