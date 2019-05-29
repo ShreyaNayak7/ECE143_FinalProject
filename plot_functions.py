1806a1aa3f79c8c06224142bd096aad49dc40432
ShreyaNayak7 <snayak@eng.ucsd.edu>
2019-05-29 00:10:50 -0700

    Updated scatter_plot to label each point with the correspondingindex of the series and to take population statistics as a series for size of bubbles

--- plot_functions.py
+++ plot_functions.py
@@ -6,16 +6,17 @@ Making Plots by Using the "matplotlib.pyplot" Module
 
 import matplotlib.pyplot as plt
 
-def scatter_plot(x,y, title):
+def scatter_plot(x,y,s,title):
     '''
     
     :param x: data along x axis
     :param y: data along y axis
     :type x: <class 'pandas.Series'>
     :type y: <class 'pandas.Series'>
+    :type s: <class 'pandas.Series'>
     '''
 
-    plt.scatter(x, y, s=y//1000, c=(y//100),alpha=0.2)
+    plt.scatter(x, y, s=s//100000, c=(y//100),alpha=0.2)
     provide_labels(x.name, y.name, title)
     for i, txt in enumerate(x.index):
         plt.annotate(txt, (x[i], y[i]))
