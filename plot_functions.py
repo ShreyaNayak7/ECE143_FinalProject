4ce85119c32c52c4e9cc3829449ae595b68cd4e5
ShreyaNayak7 <snayak@eng.ucsd.edu>
2019-06-02 15:20:40 -0700

    gives colored plots

--- plot_functions.py
+++ plot_functions.py
@@ -21,7 +21,6 @@ def scatter_plot(x,y,color,title):
     for i, txt in enumerate(x.index):
         plt.annotate(txt, (x[i], y[i]))
     plt.show()
-    # add_slider()
 
 def provide_labels(label_for_x_axis,label_for_y_axis,title):
     '''
@@ -35,26 +34,3 @@ def provide_labels(label_for_x_axis,label_for_y_axis,title):
     plt.ylabel(label_for_y_axis)
     plt.title(title)
 
-def add_slider(series_0):
-    '''
-    This function is not yet complete.
-    :type data_frame_0: <class 'pandas.DataFrame'>
-    :param data_frame_0: <class 'pandas.Series'>
-    '''
-def get_year():
-    '''
-    This function is used to obtain a year from the user.
-    '''
-    while(True):
-        year=input("Please enter the year for the corresponding scatter plot. If you do not want to enter a year, please enter \"done\". ")
-        if(year.isnumeric()):
-            try:
-                numerical_year=int(year)
-                if(numerical_year<2020 and numerical_year>1900):
-                    return numerical_year
-                else:
-                    pass
-            except:
-                pass
-        elif(year=='done'):
-            return "done"
