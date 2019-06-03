05686b7fdfa519dd505f7423dea03920926deff0
unknown <akhil.jain.dfw.allen@gmail.com>
2019-06-03 02:44:03 -0700

    A legend can be displayed.

--- plot_functions.py
+++ plot_functions.py
@@ -6,9 +6,11 @@ Making Plots by Using the "matplotlib.pyplot" Module
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.patches as mpatches
+
 # import matplotlib
 
-def scatter_plot(x,y,color,title, units_for_x="", units_for_y="",legend):
+def scatter_plot(x,y,color,title, units_for_x="", units_for_y="",legend={}):
     '''
     
     :param x: data along x axis
@@ -21,6 +23,7 @@ def scatter_plot(x,y,color,title, units_for_x="", units_for_y="",legend):
     assert isinstance(y,pd.Series)
     assert isinstance(color, pd.Series)
     assert isinstance(title, str)
+    assert isinstance(legend,dict)
     plt.scatter(x, y,c=color,alpha=0.8)
     list_of_labels=title.split(" Versus ")
     provide_labels(list_of_labels[1]+", "+units_for_x, list_of_labels[0]+", "+units_for_y, title)
@@ -28,8 +31,11 @@ def scatter_plot(x,y,color,title, units_for_x="", units_for_y="",legend):
         plt.annotate(txt, (x[i], y[i]))
     plt.box(False)
     plt.grid(True,which='both',color='0.9')
+    patches=[]
+    for counter_1 in range(len(legend)):
+        patches.append(mpatches.Patch(color=list(legend.values())[counter_1],label=list(legend.keys())[counter_1]))
+    plt.legend(handles=list(patches),loc='best')
     plt.show()
-    plt.legend(legend.keys(),legend.values())
 def provide_labels(label_for_x_axis,label_for_y_axis,title):
     '''
     
